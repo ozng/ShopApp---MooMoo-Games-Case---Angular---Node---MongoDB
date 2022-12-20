2b7ebb1b38c5b054fb0c746619054d68c63c9843
ozng <ozangurer12@gmail.com>
2022-12-20 21:43:09 +0300

    Login route

--- server/routes/user.js
+++ server/routes/user.js
@@ -36,4 +36,34 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+
+    !user && res.status(404).send("User not found");
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    !validPassword && res.status(400).send("Wrong Password");
+
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    user.token = token;
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;

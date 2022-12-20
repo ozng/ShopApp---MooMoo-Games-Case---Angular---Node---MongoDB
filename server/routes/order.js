34c5bc0099ba19ac55c1205bc27fc15218fd4249
ozng <ozangurer12@gmail.com>
2022-12-20 22:26:46 +0300

    get user order route

--- server/routes/order.js
+++ server/routes/order.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Order = require("../models/Order.js");
 const authMiddleware = require("../routes/auth");
 
+// Add Order
 router.post("/create", authMiddleware, async (req, res) => {
   const order = req.body;
 
@@ -13,6 +14,22 @@ router.post("/create", authMiddleware, async (req, res) => {
   } catch (err) {
     res.status(500).json({
       message: "Something went wrong!",
+      error: err,
+    });
+  }
+});
+
+// Get User Order
+router.get("/get/:id", async (req, res) => {
+  const userID = req.params.id;
+
+  try {
+    const orders = await Order.find({ userId: userID });
+
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json({
+      message: "Something went wrong.",
       error: err,
     });
   }

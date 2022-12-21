9ed80abd19401bf354ad1dfc734066ae90e5ba7b
ozng <ozangurer12@gmail.com>
2022-12-21 13:51:59 +0300

    Profile page

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -7,11 +7,29 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   constructor(private authService: AuthService, private route: Router) {}
 
   logoutHandler() {
     this.authService.deleteUserFromLS();
     this.route.navigate(['auth/login']);
+  }
+
+  user: any;
+
+  gender?: string;
+
+  formSubmitHandler() {}
+
+  setGender(gender: string) {
+    this.gender = gender;
+  }
+
+  ngOnInit(): void {
+    const userData = this.authService.getUser();
+
+    this.gender = userData.gender ? userData.gender : ' ';
+
+    this.user = userData;
   }
 }
